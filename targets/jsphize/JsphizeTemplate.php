eb2b6d0e671721442316e2cf690c42bd8ae13a44
Hyeonseop Jeong <gustjq1101@naver.com>
2021-05-18 01:28:11 +0900

    remove try catch statements in target php file

--- targets/jsphize/JsphizeTemplate.php
+++ targets/jsphize/JsphizeTemplate.php
@@ -4,21 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use JsPhpize\JsPhpize;
 
 function TEST_ROUTINE($input){
-    try {
-        $jsPhpize = new JsPhpize();
-        $result = $jsPhpize->renderCode($input);
-    } 
-    catch (Throwable $e) {
-        // echo "Throwable Exception!\n";
-    }
-    catch(DivisionByZeroError $e){
-        // echo "Division by Zero!\n";
-    }
-    catch(ArgumentCountError $e){
-        // echo "Argument Count Error!!\n";
-    }
-    catch(Exception $e){
-        // echo "Exception: $e->getMessage()\n\n";
-    }
+    $jsPhpize = new JsPhpize();
+    $result = $jsPhpize->renderCode($input);
 }
 
